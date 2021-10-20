cb4967d02af2ec6694c872ac02c0d5c17d67b00c
Thaylor Carvalho <thaylorcs@outlook.com>
2021-10-20 14:01:59 -0300

    App finalizado

--- calculo.php
+++ calculo.php
@@ -1,16 +1,69 @@
 <?php
 
-$distancia = $_POST['distancia'];
-$autonomia = $_POST['autonomia'];
+$mensagem = '';
 
-$valorGasolina = 4.80;
-$valorAlcool = 3.80;
-$valorDiesel = 3.90;
+if ($_POST) {
 
-$calculoGasolina = ($distancia / $autonomia) * $valorGasolina;
-$calculoAlcool = ($distancia / $autonomia) * $valorAlcool;
-$calculoDiesel = ($distancia / $autonomia) * $valorDiesel;
+ $distancia = $_POST['distancia'];
+ $autonomia = $_POST['autonomia'];
 
-echo "<p>O valor do consumo em R$ para Gasolina foi de: R$" .$calculoGasolina."</p>";
-echo "<p>O valor do consumo em R$ para Álcool foi de: R$" .$calculoAlcool."</p>";
-echo "<p>O valor do consumo em R$ para Álcool foi de: R$" .$calculoDiesel."</p>";
+ if (is_numeric($distancia) && is_numeric($autonomia)) {
+
+  if ($distancia > 0 && $autonomia > 0) {
+
+   $valorGasolina = 6.10;
+   $valorAlcool = 3.80;
+   $valorDiesel = 3.90;
+
+   $calculoGasolina = ($distancia / $autonomia) * $valorGasolina;
+   $calculoGasolina = number_format($calculoGasolina, 2, ',', '.');
+   $calculoAlcool = ($distancia / $autonomia) * $valorAlcool;
+   $calculoAlcool = number_format($calculoAlcool, 2, ',', '.');
+   $calculoDiesel = ($distancia / $autonomia) * $valorDiesel;
+   $calculoDiesel = number_format($calculoDiesel, 2, ',', '.');
+
+   $mensagem .= "<div class='sucesso'>";
+   $mensagem .= "<p>O valor total será de: </p>";
+   $mensagem .= "<ul>";
+   $mensagem .= "<li>O valor do consumo em R$ para Gasolina foi de: R$" . $calculoGasolina . "</li>";
+   $mensagem .= "<li>O valor do consumo em R$ para Álcool foi de: R$" . $calculoAlcool . "</li>";
+   $mensagem .= "<li>O valor do consumo em R$ para Diesel foi de: R$" . $calculoDiesel . "</li>";
+   $mensagem .= "</ul>";
+   $mensagem .= "</div>";
+  } else {
+   $mensagem .= "<div class='erro'>";
+   $mensagem .= "<p> O valor da distância e da autonomia devem ser maior que 0.</p>";
+   $mensagem .= "</div>";
+  }
+ } else {
+  $mensagem .= "<p> O valor recebido não é númerico. </p>";
+ }
+
+} else {
+ echo "<p> Nenhum dado foi recebido pelo formulário.";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cálculo de consumo de combustível</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <main>
+        <div class="painel">
+            <h2>Resultado do cálculo de consumo</h2>
+            <div class="conteudo-painel">
+                <?php
+echo $mensagem;
+?>
+                <a class="botao" href="index.php">Voltar </a>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
--- index.php
+++ index.php
@@ -6,33 +6,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cálculo de consumo de combustível</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
     <main>
-        <h1>Instruções</h1>
+        <div class="painel">
+            <h2>Instruções</h2>
 
-        <p>Esta aplicação tem como finalidade demonstrar os valores que serão gastos com combustível durante uma viagem,
-            com
-            base no consumo do seu veículo, e com a distância determinada por você!</p>
+            <div class="conteudo-painel">
+                <p>Esta aplicação tem como finalidade demonstrar os valores que serão gastos com combustível durante uma
+                    viagem,
+                    com
+                    base no consumo do seu veículo, e com a distância determinada por você!</p>
 
-        <p>Os combustíveis disponíveis para este cálculo são:</p>
+                <p>Os combustíveis disponíveis para este cálculo são:</p>
 
-        <ul>
-            <li>Álcool</li>
-            <li>Diesel</li>
-            <li>Gasolina</li>
-        </ul>
+                <ul>
+                    <li>Álcool</li>
+                    <li>Diesel</li>
+                    <li>Gasolina</li>
+                </ul>
+            </div>
 
+        </div>
         <div class="painel">
             <h2>Cálculo do valor (R$) do consumo</h2>
             <div class="conteudo-painel">
                 <form action="calculo.php" method="POST">
                     <label for="distancia">Distância em KM a ser percorrida</label>
-                    <input type="text" name="distancia" />
+                    <input class="campoTexto" type="number" name="distancia" required />
 
                     <label for="autonomia">Consumo de combustível do veículo (Km/L) </label>
-                    <input type="text" name="autonomia">
+                    <input class="campoTexto" type="number" name="autonomia" required>
 
                     <button class="botao" type="submit">Calcular</button>
                 </form>
